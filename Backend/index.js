83bf7b79c8de32313bab0ebe93bdd00b0c1a0c33
Ugam Raj <ugamrajseervi1994@gmail.com>
2024-10-18 08:16:57 +0530

    added comment in index file

--- Backend/index.js
+++ Backend/index.js
@@ -20,16 +20,18 @@ const PORT = process.env.PORT || 10000;
 const DATABASE_URL = process.env.DATABASE_URL;
 const DB_NAME = process.env.DB_NAME;
 
-app.use(cookieParser());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(fileUpload({ useTempFiles: true, tempFileDir: "/tmp/" }));
+// Middleware setup
+app.use(cookieParser()); // To parse cookies
+app.use(express.json()); // To parse JSON request bodies
+app.use(express.urlencoded({ extended: true })); // To parse URL-encoded request bodies
+app.use(fileUpload({ useTempFiles: true, tempFileDir: "/tmp/" })); // To handle file uploads and temporary file storage
 
 // app.use(cors()); // for all url applicable
 const corsOptions = {
+  //!  cross origin with specific domain
   origin: [process.env.FRONTED_URL, process.env.DASHBORAD_URL],
   methods: "GET,PUT,PATCH,POST,DELETE",
-  credentials: true,
+  credentials: true, // To allow cookies and other credentials
   optionsSuccessStatus: 200,
 };
 
@@ -39,6 +41,7 @@ app.use("/api/v1/message", messageRoute);
 app.use("/api/v1/user", userRoute);
 app.use("/api/v1/appointment", appointmentRoute);
 
+// connect data base
 connectDB(DATABASE_URL, DB_NAME);
 
 //! Image Storage Engine
@@ -49,6 +52,7 @@ cloudinary.v2.config({
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
+// Error handling middleware
 app.use(errorMiddleware);
 
 app.listen(PORT, () => {
